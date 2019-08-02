54dde8bf3394da58fb7514416ce3a0b75d1d173c
Spidy20 <kushalbhavsar58@gmail.com>
2019-08-02 18:29:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+
+app = Flask(__name__)
+
+
+MODEL_PATH = 'model_resnet.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+model._make_predict_function()
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x, mode='caffe')
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/Image_Classification', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)
+        pred_class = decode_predictions(preds, top=1)
+        result = str(pred_class[0][0][1])
+        return result
+    return None
+
+if __name__ == '__main__':
+      app.run(debug=True)
